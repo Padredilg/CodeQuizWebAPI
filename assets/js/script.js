4d7106c61d869d8a000e2635a60da59a46d613f9
Luiz P <padredilg@knights.ucf.edu>
2021-07-18 14:25:47 -0400

    Added buttons and declared button variables in JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//this var represents the highscores button
+var viewHighScoresBtn = document.querySelector("#highscores");
+
+//this var represents the Take Quiz button
+var takeQuizBtn = document.querySelector("#takeQuiz");
+
+//function generateHighScores {
+    //write function to generate highscores in the main.  
+//}
+
+//function startQuiz {
+    //write function to start quiz in the main.
+//}
+  
+//when view Highest Scores is clicked, show High scores
+viewHighScoresBtn.addEventListener("click", generateHighScores);
+
+//when Take Quiz is clicked, Start Quiz
+viewHighScoresBtn.addEventListener("click", startQuiz);
