939e4216996ef85b60c4bd09170b43ad979bae49
Luiz P <padredilg@knights.ucf.edu>
2021-07-18 19:04:48 -0400

    created highscores page and fixed footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,12 @@
-//this var represents the highscores button
-var viewHighScoresBtn = document.querySelector("#highscores");
-
 //this var represents the Take Quiz button
 var takeQuizBtn = document.querySelector("#take-quiz");
 
-//function generateHighScores {
-    //write function to generate highscores in the main.  
-//}
-
-//function startQuiz {
-    //write function to start quiz in the main.
-//}
-  
-//when view Highest Scores is clicked, show High scores
-viewHighScoresBtn.addEventListener("click", generateHighScores);
+var startQuiz = function() {
+    console.log("Take Quiz was clicked!")
+}
 
 //when Take Quiz is clicked, Start Quiz
-viewHighScoresBtn.addEventListener("click", startQuiz);
+takeQuizBtn.addEventListener("click", startQuiz);
 
 //We can make a prepare yourself countdown when quiz will start,
 //and only then quiz starts and timer starts
