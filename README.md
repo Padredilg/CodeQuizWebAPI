17b0822066a542e8117b6f1219c46df7e10b50c2
Luiz P <padredilg@knights.ucf.edu>
2021-07-18 20:46:24 -0400

    created startQuiz function for when take quiz button is pressed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CodeQuizWebAPI
 
-Include screenshotss of the website and also the link to the live page
+Include screenshots of the website and also the link to the live page
 
 <a href='https://www.freepik.com/vectors/education'>Education vector created by pch.vector - www.freepik.com</a>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
 //this var represents the Take Quiz button
 var takeQuizBtn = document.querySelector("#take-quiz");
+var buttonsEl = document.querySelector("#buttons");
 
 var startQuiz = function() {
-    console.log("Take Quiz was clicked!")
+    //remove takeQuiz button
+    takeQuizBtn.remove();
+
+    document.querySelector("#question").textContent = "Quiz will start in 10";
+
+    var startingIn = 10;
+    var interval = setInterval(function(){
+        startingIn --;
+        if (startingIn === 0) {
+            clearInterval(interval);
+            questions();
+        }
+        else{
+            document.querySelector("#question").textContent = "Quiz will start in " + startingIn;
+        }
+    },1000);   
+}
+
+var questions = function(){
+    document.querySelector("#question").textContent = "Sample Question 1";
+
+    var choiceEl = document.createElement("button")
+    choiceEl.className = "choice";
+    choiceEl.textContent = "Sample Button";
+
+    buttonsEl.appendChild(choiceEl);
 }
 
 //when Take Quiz is clicked, Start Quiz
