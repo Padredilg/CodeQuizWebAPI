93a4eee73d169a83d0f0729fdb3f93364b0ac8cb
Luiz P <padredilg@knights.ucf.edu>
2021-07-18 21:53:13 -0400

    Added sample buttons and questions and adjusted their styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var startQuiz = function() {
 
     document.querySelector("#question").textContent = "Quiz will start in 10";
 
-    var startingIn = 10;
+    var startingIn = 1;
     var interval = setInterval(function(){
         startingIn --;
         if (startingIn === 0) {
@@ -22,13 +22,68 @@ var startQuiz = function() {
 }
 
 var questions = function(){
-    document.querySelector("#question").textContent = "Sample Question 1";
+    var numQuestions = 4;
+    var choice1El;
+    var choice2El;
+    var choice3El;
+    var choice4El;
 
-    var choiceEl = document.createElement("button")
-    choiceEl.className = "choice";
-    choiceEl.textContent = "Sample Button";
+    //loop through questions and collect answers for score;
+    //maybe logic for score is to add an eventListener in each case and then use if statement to 
+    //compare if clicked was the right choice, then inside the if include the image and add to score
+    //eventually add logic to randomize question order and choices order
+    for(var i=1; i<=numQuestions; i++){
+        switch (i) {
+            case 1:
+                document.querySelector("#question").textContent = "Sample Question 1";
 
-    buttonsEl.appendChild(choiceEl);
+                //button 1                
+                choice1El = document.createElement("button")
+                choice1El.className = "choice";
+                choice1El.textContent = "Sample Button 1";
+                buttonsEl.appendChild(choice1El);
+
+                //button 2               
+                choice2El = document.createElement("button")
+                choice2El.className = "choice";
+                choice2El.textContent = "Sample Button 2";
+                buttonsEl.appendChild(choice2El);
+
+                //button 3          
+                choice3El = document.createElement("button")
+                choice3El.className = "choice";
+                choice3El.textContent = "Sample Button 3";
+                buttonsEl.appendChild(choice3El);
+
+                //button 4
+                choice4El = document.createElement("button")
+                choice4El.className = "choice";
+                choice4El.textContent = "Sample Button 4";
+                buttonsEl.appendChild(choice4El);
+
+                break;
+            case 2:
+                document.querySelector("#question").textContent = "Sample Question 2";
+                
+                break;
+            case 3:
+                document.querySelector("#question").textContent = "Sample Question 3";
+
+                break;
+            case 4:
+                document.querySelector("#question").textContent = "Sample Question 4";
+
+                break;
+          }
+    }
+
+    // document.querySelector("#question").textContent = "Sample Question 1";
+
+    // var choiceEl = document.createElement("button")
+    // choiceEl.className = "choice";
+    // choiceEl.textContent = "Sample Button";
+
+    // buttonsEl.appendChild(choiceEl);
 }
 
 //when Take Quiz is clicked, Start Quiz
